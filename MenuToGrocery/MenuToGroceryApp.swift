a8daa11143afb8f798152d20693f37a4a637ee81
TheGatesOfParadise <109245867+TheGatesOfParadise@users.noreply.github.com>
2023-04-17 00:04:19 -0400

    Initial Commit

---
+++ MenuToGrocery/MenuToGroceryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuToGroceryApp.swift
+//  MenuToGrocery
+//
+//  Created by Mom macbook air on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct MenuToGroceryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
